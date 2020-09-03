d5cbb4dbb50dcd54e35625b266c1aa986a38c4ee
Burgentie <63803079+Burgentie@users.noreply.github.com>
2020-09-03 16:04:02 +0200

    Update Members.js

--- commands/Members.js
+++ commands/Members.js
@@ -15,6 +15,6 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "members",
+    name: "Members",
     description: "how many people are in the server"
 }
