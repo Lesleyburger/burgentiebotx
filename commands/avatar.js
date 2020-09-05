08ee23d2e111ddde1a113ec1e3c45f255c311e9d
Burgentie <63803079+Burgentie@users.noreply.github.com>
2020-09-05 18:54:19 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,13 +5,13 @@ module.exports.run = async (bot, message, args) => {
     var user = message.mentions.users.first();
 
     var embed = new discord.RichEmbed()
-    embed.setImage(user.avatarURL)
-    .setColor('#275BF0')
+        .setImage(user.avatarURL)
+        .setColor('#275BF0')
 
     return message.channel.send(embed);
 }
 
 module.exports.help = {
     name: "avatar",
-    description: "Info about the server"
+    description: "avatar of from a user"
 }
